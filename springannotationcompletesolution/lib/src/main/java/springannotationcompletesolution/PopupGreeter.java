768eb2406f135487afdccb75f40055fcf058cb06
Aman singh patel <98939571+aspraj@users.noreply.github.com>
2022-03-05 12:35:09 +0530

    Add files via upload

--- springannotationcompletesolution/lib/src/main/java/springannotationcompletesolution/PopupGreeter.java
+++ springannotationcompletesolution/lib/src/main/java/springannotationcompletesolution/PopupGreeter.java
@@ -6,10 +6,11 @@ import javax.swing.JOptionPane;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
 @Component
 public class PopupGreeter implements Greeter {
 	private Visitor visitor;
-	
+
 	public Visitor getVisitor() {
 		return visitor;
 	}
@@ -21,7 +22,7 @@ public class PopupGreeter implements Greeter {
 	 * called Spring bean autowiring.
 	 */
 	@Autowired
-	@Qualifier("jerseyVisitor") //This is used for setter based DI
+	@Qualifier("jerseyVisitor") // This is used for setter based DI
 	public void setVisitor(Visitor visitor) {
 		System.out.println("visitor set to " + visitor);
 		this.visitor = visitor;
