282dcaec872fa4e722746eb0a951751d44dd8619
Aman singh patel <98939571+aspraj@users.noreply.github.com>
2022-03-15 07:32:57 +0530

    practice string

---
+++ test/src/practice/string/day4/StringAllMethods.java
@@ -0,0 +1,56 @@
+package practice.string.day4;
+
+public class StringAllMethods {
+
+	public static void main (String args[])
+	{
+		String ankita = " Ankita Patel ";
+		String aman = " Aman Singh Patel ";
+		String ankit = "Ankita patel";
+		String yasoda = "Ankita Patel";
+		String whiteSpace = " ";
+		String blank ="";
+
+		System.out.println(aman);// print string aman 
+//		System.out.println(ankita);//print string ankita
+//		System.out.println(ankita.charAt(2));// character at 2
+//		System.out.println(ankita.compareTo(aman));// false 1
+//		System.out.println(ankita.compareTo(ankit));//-33
+//		System.out.println(ankita.compareToIgnoreCase(ankit));//-65
+//		System.out.println(ankita.concat(ankit));//add two string 
+//		System.out.println(ankita.contains(yasoda));//ankita contains string yasoda or not
+//		System.out.println(ankita.endsWith("Patel"));//ankita string ends with Patel or not
+//		System.out.println(ankita.equals(yasoda));//Compare two strings they are same or not
+//		System.out.println(ankita.equalsIgnoreCase(ankit));//Compare two strings they are same or not
+//		System.out.println(ankita.hashCode());// hashcode print karega
+//		System.out.println(ankita.indent(5));// starting m space add karega
+//		System.out.println(ankita.indexOf('l'));//l ka index no kya h bo bataega
+//		System.out.println(ankita.indexOf("Patel"));//string  ka index kya h string m
+//		System.out.println(ankita.indexOf('a', 4));//starting 4 index ke bad a konse index p h
+//		System.out.println(ankita.indexOf('a', 6));//starting 6 index ke bad a konse index p h
+//		System.out.println(ankita.indexOf('a', 10));//10 index ke bad a konse index p h
+//		System.out.println(aman.indexOf("man",0 ));//string 0 index ke bad kahaan present h
+//		System.out.println(aman.indexOf("man",2 ));//same
+//		System.out.println(aman.isBlank());// string blank h ya nhi
+//		System.out.println(blank.isBlank());// string blank h ya nhi
+//		System.out.println(aman.isEmpty());// string empty h ya nhi
+//		System.out.println(whiteSpace.isEmpty());// string empty h ya nhi
+//		System.out.println(blank.isEmpty());// string blank h ya nhi
+//		System.out.println(aman.length());// length of string kya h
+//		System.out.println(aman.matches(aman));//string se string match kar raha h ya nhi
+//		System.out.println(aman.repeat(3));//printing same string 3 times
+//		System.out.println(aman.replace('a', 'p'));//replacing all the  char in string
+//		System.out.println(aman.replace(aman, ankita));//the whole string wil be replaced
+//		System.out.println(aman.replaceFirst(aman, "asp"));//same
+//		System.out.println(aman.startsWith("Aman"));// string aman se start horai h ya nhi
+//		System.out.println(whiteSpace.strip()); //remove white spaces from the start
+//		System.out.println(aman.stripIndent());//remove white spaces from the both
+//		System.out.println(aman.stripLeading());// leading spaces ko remove kardega
+//		System.out.println(aman.stripTrailing());// ending spaces ko remove kardega
+//		System.out.println(aman.substring(6));// print karega 6th index ke bad ke elements
+//		System.out.println(aman.substring(3, 9));//print karega 3rd index se 9th tk
+//		System.out.println(aman.toLowerCase());// sabhi elements ko lowercase m kardega
+//		System.out.println(aman.toUpperCase());//sabhi ko upppercase m kardega
+//		System.out.println(aman.trim());// trim karega both side se remove white spaces
+	}
+}
